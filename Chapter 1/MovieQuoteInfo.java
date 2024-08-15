b24609f65f55534374eb1e4848e34f4b44db9db5
aprildde <156050739+aprildde@users.noreply.github.com>
2024-08-16 00:45:12 +0800

    Add files via upload

---
+++ Chapter 1/MovieQuoteInfo.java
@@ -0,0 +1,10 @@
+public class MovieQuoteInfo {
+   public static void main (String[] args){
+
+        System.out.println("Movie Quote: \"Laugh, and the world laughs with you. Weep, and you weep alone.\"");
+        System.out.println("Movie Title: Oldboy (Korean Movie)");
+        System.out.println("Said by: Oh Dae-su, the main character (played by Choi Min-sik)");
+        System.out.println("Year of the Movie: 2003");
+
+    }
+}
