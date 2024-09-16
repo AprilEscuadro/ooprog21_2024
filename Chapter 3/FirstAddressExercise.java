33bd8ea345132701a1ecff7841f54d820eff2ca9
AprilEscuadro <156050739+AprilEscuadro@users.noreply.github.com>
2024-09-16 20:29:58 +0800

    Add files via upload

---
+++ Chapter 3/FirstAddressExercise.java
@@ -0,0 +1,14 @@
+public class FirstAddressExercise {
+    
+    public static void displayAddress() {
+        System.out.println("Samsung Electronics");
+        System.out.println("129 Samsung-ro");
+        System.out.println("Yeongtong-gu, Suwon-si, Gyeonggi-do, 16677");
+        System.out.println("Company: Samsung Electronics");
+    }
+
+    
+    public static void main(String[] args) {
+        displayAddress();
+    }
+}
